deadb2e8e3ba02150f71b7424ec7ae1266167535
elGOATdelcode <keantatum84@proton.me>
2024-12-04 13:40:18 -0600

    a

--- src/models/db.js
+++ src/models/db.js
@@ -1,17 +1,22 @@
-
 const mysql = require('mysql2');
 const dotenv = require('dotenv');
 
 dotenv.config();
 
-const pool = mysql.createPool({
+const connection = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0
+  connectTimeout: 10000,  
 });
 
-module.exports = pool.promise();
+connection.connect((err) => {
+  if (err) {
+    console.error('Error de conexión:', err);
+  } else {
+    console.log('Conexión exitosa');
+  }
+});
+
+module.exports = connection.promise(); 
